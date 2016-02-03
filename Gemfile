c43eb5972442c372e895986b61e21445e8492d4b
harrisonlim <harrison.lim.hx@gmail.com>
2016-02-03 06:34:35 +0000

    added devise gem for user authentication, set up routes for users

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'bootstrap-sass', '~> 3.3.4.1'
 # Use Sprockets gem
 gem 'sprockets', '2.11.0'
 
+# Use Devise gem for user authentication
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Flixter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Default URL option for Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
 end
