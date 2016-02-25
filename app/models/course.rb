9f8a05597497bd9c97cc5086f452c05b41737083
harrisonlim <harrison.lim.hx@gmail.com>
2016-02-25 19:57:38 +0000

    added jquery ui rails gem, added functionality to allow dragging and reordering of lessons in instructor page

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,18 +2,18 @@ class Course < ActiveRecord::Base
 	belongs_to :user
 	has_many :sections
 	has_many :enrollments
-	
+
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 	# validates :image, presence: true
-	
+
 	mount_uploader :image, ImageUploader
-	
+
 	def free?
 		cost.zero?
 	end
-	
+
 	def premium?
 		! free?
 	end
