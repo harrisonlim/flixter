a28ca9b0004ee68e5b6eaa920cbce6ad63e464cd
harrisonlim <harrison.lim.hx@gmail.com>
2016-03-04 08:55:15 +0000

    added dashboards controller, created user dashboard view and styled the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Flixter::Application.routes.draw do
 
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :photos, only: [:create]
